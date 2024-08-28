ca496ec009e4723e897c2d80fa9047829d06c2da
theprogrammer006 <theprogrammer.giftreeng@gmail.com>
2024-08-28 18:22:32 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://test:test@dpg-cr0cao88fa8c73d2i1s0-a.oregon-postgres.render.com/gia_server"
+SQLALCHEMY_DATABASE_URL = "postgresql://gia_server_user:Tch6NGGOLgHTo8ppkbeQEQS2lvuQhN9m@dpg-cr0cao88fa8c73d2i1s0-a.oregon-postgres.render.com/gia_server"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
